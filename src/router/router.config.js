fc6bb4ed85e7a6d2b5fa7ff8397a423555f3dc4c
Mydekaifa <1012737795@qq.com>
2018-12-27 14:10:16 +0800

    路由文件

---
+++ src/router/router.config.js
@@ -0,0 +1,23 @@
+import homeview from '../components/home.vue';
+import targetview from '../components/targets.vue';
+import memoview from '../components/memo.vue';
+import myview from '../components/my.vue';
+
+export default[
+    {
+        path:"/home",
+        component:homeview
+    },
+    {
+        path:"/target",
+        component:targetview
+    },
+    {
+        path:"/memo",
+        component:memoview
+    },
+    {
+        path:"/my",
+        component:myview
+    }
+]
