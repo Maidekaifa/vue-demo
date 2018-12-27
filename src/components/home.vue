0b607f928dd049a5a1075b3cc2c25f6408cb9229
Mydekaifa <1012737795@qq.com>
2018-12-27 16:16:34 +0800

    banner图

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,21 +1,57 @@
 <template>
     <div>
-         <mt-button @click="handleClick">按钮</mt-button>
+        <div class="swiper-container">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide">
+                    <img src="../assets/img/banner1.jpg" class="img-responsive center-block" />
+                </div>
+                <div class="swiper-slide">
+                	<img src="../assets/img/banner1.jpg" class="img-responsive center-block" />
+                </div>
+                <div class="swiper-slide">
+                	<img src="../assets/img/banner1.jpg" class="img-responsive center-block" />
+                </div>
+                <div class="swiper-slide">
+                	<img src="../assets/img/banner1.jpg" class="img-responsive center-block" />
+                </div>
+            </div>
+        </div>
     </div>
 </template>
-<style>
-@import url("//unpkg.com/mint-ui/lib/style.css");
-</style>
 <script>
-import {  Toast } from 'mint-ui';
-export default {
-    methods:{
-        handleClick(){
-            alert(1);
+import swiper from 'swiper';
+import '../assets/js/jquery-1.11.3.min';
+export default{
+    data(){
+        return{
+            swiper:null
         }
+    },
+    mounted(){
+        this.swiper= new Swiper('.swiper-container',{
+        	loop:true,
+            effect : 'cube',
+  			cubeEffect: {
+			    slideShadows: true,
+			    shadow: true,
+			    shadowOffset: 100,
+			    shadowScale: 0.6
+  			},
+        })
     }
+    
     
 }
 </script>
+<style>
+@import url("../assets/css/swiper.css");
+.swiper-container{
+    width: 100%;
+}
+.swiper-slide{
+    width: 100%;
+}
+
+</style>
 
 
