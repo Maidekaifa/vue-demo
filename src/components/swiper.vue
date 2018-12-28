3fd2949e218daf7ce98b7ba530cec49eb145f68f
Mydekaifa <1012737795@qq.com>
2018-12-28 17:41:29 +0800

    target页面

---
+++ src/components/swiper.vue
@@ -0,0 +1,58 @@
+<template>
+	<div>
+		<!--banner图部分-->
+		
+        <div class="swiper-container" style="background: #FFFFFF;">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide">
+                    <img src="../assets/img/banner1.jpg" alt="banner" class="img-responsive center-block" />
+                </div>
+                <div class="swiper-slide">
+                	<img src="../assets/img/banner1.jpg" alt="banner" class="img-responsive center-block" />
+                </div>
+                <div class="swiper-slide">
+                	<img src="../assets/img/banner1.jpg" alt="banner" class="img-responsive center-block" />
+                </div>
+                <div class="swiper-slide">
+                	<img src="../assets/img/banner1.jpg" alt="banner" class="img-responsive center-block" />
+                </div> 
+            </div>
+        </div>
+	</div>
+</template>
+
+<script>
+import swiper from 'swiper';
+export default{
+	data(){
+        return{
+            swiper:null
+        }
+    },
+    mounted(){
+    	this.swiper= new Swiper('.swiper-container',{
+        	autoplay:3000,
+        	loop:true,
+            effect : 'cube',
+  			cubeEffect: {
+			    slideShadows: true,
+			    shadow: true,
+			    shadowOffset: 100,
+			    shadowScale: 0.6
+  			},
+        });
+    }
+    
+}
+
+</script>
+
+<style>
+	@import url("../assets/css/swiper.css");
+.swiper-container{
+    width: 100%;
+}
+.swiper-slide{
+    width: 100%;
+}
+</style>
--- src/components/targets.vue
+++ src/components/targets.vue
@@ -1,5 +1,68 @@
 <template>
     <div>
-        <p>我是目标页</p>
+    	<div class="container-fluid text-center">
+    		<h2>What to do today?</h2>
+    	</div>
+    	<div class="row container-fluid">
+	        <div class="col-lg-6" id="inputTxt">
+	    		<div class="input-group">
+	      			<input id="vals" @focus="focusTxt" type="text" class="form-control" placeholder="Do today...">
+	      				<span class="input-group-btn">
+	        				<button @click="add" class="btn btn-default" type="button">添加</button>
+	      				</span>
+	    		</div><!-- /input-group -->
+	  		</div><!-- /.col-lg-6 -->
+	  	</div>
+	  	<div id="bbs" class="container-fluid">
+	  		<div id="finish">
+	  			<p class="complete">finish</p>
+	  		</div>
+	  		<div id="unfinished">
+	  			<p class="complete">unfinished</p>
+	  		</div>
+	  	</div>
     </div>
 </template>
+<script>
+	export default{
+		methods:{
+			focusTxt(){
+				let inputs=$("#vals").val();
+				////////////////////////////////////做到这里
+			},
+			add(){
+				let valsTxt=$("#vals").val();
+				let str='<div class="show text-center">';
+				str+='<span class="close"><i class="iconfont icon-Tick"></i></span>';
+				str+=valsTxt;
+				str+="</div>"
+				let shows=$(".show").text();
+				if(valsTxt!=shows){
+					$("#unfinished").append(str);
+				}else{
+					alert("该目标今天您已经记录啦");
+				}
+			}
+		}
+	}
+</script>
+<style>
+*{
+	margin:0;
+	padding: 0;
+}
+#inputTxt{
+	margin-top: 10px;
+}
+#bbs{
+	margin-top: 50px;
+}
+.complete{
+	color: #CCCCCC;
+	font-style: italic;
+}
+.close i{
+	color: #00BC9B;
+	font-weight: bold;
+}
+</style>
