f5eff8dc61ab7bac4dafcc16818fb6c0e09f027b
Mydekaifa <1012737795@qq.com>
2019-01-03 18:06:10 +0800

    解决vue单页面应用刷新网页后vuex的state数据丢失这个bug

--- src/components/header.vue
+++ src/components/header.vue
@@ -13,6 +13,18 @@ import {mapGetters} from "vuex";
 	export default{
 		
 		computed:mapGetters(["headerTxt","QR_Code","returns"]),
+
+		//用来解决vue单页面应用刷新网页后Vuex的state数据丢失的问题created()
+		created () {
+    		//在页面加载时读取sessionStorage里的状态信息
+		    if (sessionStorage.getItem("store") ) {
+		        this.$store.replaceState(Object.assign({}, this.$store.state,JSON.parse(sessionStorage.getItem("store"))))
+		    } 
+		    //在页面刷新时将vuex里的信息保存到sessionStorage里
+		    window.addEventListener("beforeunload",()=>{
+		        sessionStorage.setItem("store",JSON.stringify(this.$store.state))
+		    })
+		},
 		
 		watch:{
 			$route(to){
