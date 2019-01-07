391786698b8aff89ac32034b9fe6695a3457fe66
Mydekaifa <1012737795@qq.com>
2019-01-07 11:47:18 +0800

    注释

--- src/main.js
+++ src/main.js
@@ -1,18 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
+
+//引入Mint-ui组件
 import Mint from 'mint-ui'
 Vue.use(Mint);
 
+//引入路由
 import VueRouter from 'vue-router'
 Vue.use(VueRouter);
 
 import routes from './router/router.config'
 
-import store from './store/index.js'
-
 const router=new VueRouter({
   routes
 })
+
+//引入vuex
+import store from './store/index.js'
 
 //引入自己定义的div和a组件
 import MYDIV from './components/common/my-div'
